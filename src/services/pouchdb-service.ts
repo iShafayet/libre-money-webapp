a496a572e7429cdda2ecddf67dfdcfb56c458375
Sayem Shafayet <ishafayet@gmail.com>
2023-06-28 15:49:02 +0600

    Add intentional delay to simulate potential processing lag

--- src/services/pouchdb-service.ts
+++ src/services/pouchdb-service.ts
@@ -1,3 +1,5 @@
+import { sleep } from "src/utils/misc-utils";
+
 const LOCAL_DB_NAME = "cash-keeper-main";
 
 const pouchdb = new PouchDB(LOCAL_DB_NAME);
@@ -15,12 +17,30 @@ async function createCollectionIndexIfNeeded() {
   collectionIndexCreated = true;
 }
 
+const knownTemporaryFields = ["_currencySign"];
+
+function stripKnownTemporaryFields(doc: any) {
+  knownTemporaryFields.forEach((field) => {
+    delete doc[field];
+  });
+}
+
+const INTENTIONAL_DELAY = 300;
+
+async function delayIntentionally() {
+  await sleep(INTENTIONAL_DELAY);
+}
+
 export const pouchdbService = {
   getDb() {
     return pouchdb;
   },
 
   async upsertDoc(doc: PouchDB.Core.PostDocument<any>) {
+    await delayIntentionally();
+
+    doc = JSON.parse(JSON.stringify(doc));
+    stripKnownTemporaryFields(doc);
     if (doc._id) {
       return pouchdb.put(doc);
     } else {
@@ -29,12 +49,16 @@ export const pouchdbService = {
   },
 
   async listDocs() {
+    await delayIntentionally();
+
     return await pouchdb.allDocs({
       include_docs: true,
     });
   },
 
   async listByCollection(collectionName: string) {
+    await delayIntentionally();
+
     await createCollectionIndexIfNeeded();
 
     return await pouchdb.find({
@@ -45,10 +69,14 @@ export const pouchdbService = {
   },
 
   async getDocById(_id: string) {
+    await delayIntentionally();
+
     return await pouchdb.get(_id);
   },
 
   async removeDoc(doc: PouchDB.Core.PostDocument<any>) {
+    await delayIntentionally();
+
     return await pouchdb.remove(doc._id, doc._rev);
   },
 };
