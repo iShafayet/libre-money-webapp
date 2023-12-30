71c624eddcfdd1c4f25f1f31d03ed5b8a4b33e31
Sayem Shafayet <ishafayet@gmail.com>
2023-12-30 15:06:37 +0600

    Increment patch version

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -192,7 +192,7 @@ export default defineComponent({
 
     const userStore = useUserStore();
 
-    const appVersion = "0.1.2";
+    const appVersion = "0.1.3";
     const internalBuild = "DEV_BUILD";
     const buildDate = "NOT_APPLICABLE";
 
